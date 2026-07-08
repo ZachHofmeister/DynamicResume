0c62a06acd5cbe8f762355eda37739e0758c33bf
ZachHofmeister <17202322+ZachHofmeister@users.noreply.github.com>
2026-07-07 21:51:03 -0600

    started copying over current resume format to practice tailwindcss and as a base for creating the customizer

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <Page>
+      <h1 className="text-center font-serif font-bold text-3xl tracking-tight">Zach Hofmeister</h1>
+      <p className="text-center font-serif text-sm">
+        Grand Junction, CO | zach@email.com | (123) 123-4567 | LinkedIn | GitHub
+      </p>
+      <section className="mt-5">
+        <h2 className="font-serif font-bold text-xl text-green-700">Education</h2>
+        <hr className="border-t-2 mb-1.5"/>
+        <div className="flex flex-row justify-between">
+          <h3 className="font-serif font-bold tracking-tight">BS in Computer Science | California State University, Fullerton</h3>
+          <h3 className="font-serif font-bold tracking-tight">August 2018 - May 2022</h3>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </section>
+    </Page>
+  );
+}
+
+function Page({children}: {children: React.ReactNode}) {
+  return (
+    <div className="m-20 p-10 aspect-8.5/11 inset-ring inset-ring-blue-500">
+      {children}
     </div>
   );
 }
