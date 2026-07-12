83e206361920a89a417abbc658bd4a813d32fbe8
ZachHofmeister <17202322+ZachHofmeister@users.noreply.github.com>
2026-07-11 21:35:48 -0600

    pulled job entries out into a Job component with JobInfo interface

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import type { ReactNode } from "react";
 
 export default function Home() {
+  return (<Resume></Resume>);
+}
+
+function Resume() {
   return (
     <Page>
       <h1 className="text-center font-serif font-bold text-3xl tracking-tight text-green-700">Zach Hofmeister</h1>
@@ -20,60 +24,28 @@ export default function Home() {
         </ul>
       </Section>
       <Section title="Experience">
-        <div className="my-2">
-          <div className="flex flex-row justify-between">
-            <h3 className="font-serif font-bold tracking-tight">IT Technician | Amentum | Grand Junction, CO</h3>
-            <h3 className="font-serif font-bold tracking-tight">Aug 2025 - Present</h3>
-          </div>
-          <ul className="list-disc list-inside">
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Deployed Windows and iOS devices for an organization of 350+ users with varied configuration requirements
-            </li>
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Responded to user questions and provided solutions to software and hardware problems in person and remotely
-            </li>
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Developed and maintained PowerShell scripts to streamline device deployments
-            </li>
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Collaborated with team members to document solutions and share best practices
-            </li>
-          </ul>
-        </div>
-        <div className="my-2">
-          <div className="flex flex-row justify-between">
-            <h3 className="font-serif font-bold tracking-tight">Campus Missionary | FOCUS | Grand Junction, CO</h3>
-            <h3 className="font-serif font-bold tracking-tight">May 2022 - Jun 2025</h3>
-          </div>
-          <ul className="list-disc list-inside">
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Mentored 100+ college students in spiritual growth, leadership and personal excellence
-            </li>
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Built and maintained a dynamic donor-facing newsletter website to increase engagement and retention
-            </li>
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Secured full funding for role ($140K+ in 3 years) by cultivating relationships with 60+ recurring donors
-            </li>
-          </ul>
-        </div>
-        <div className="my-2">
-          <div className="flex flex-row justify-between">
-            <h3 className="font-serif font-bold tracking-tight">Coding Instructor | Code Ninjas | San Diego, CA / Remote</h3>
-            <h3 className="font-serif font-bold tracking-tight">Jun 2021 - May 2022</h3>
-          </div>
-          <ul className="list-disc list-inside">
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Delivered engaging coding lessons to 30+ students (ages 7-14) in a fast-paced setting
-            </li>
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Taught core and advanced concepts in JavaScript, HTML/CSS, C#, Java, and Dart
-            </li>
-            <li className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
-              Quickly adopted new technologies to meet evolving curriculum and student needs
-            </li>
-          </ul>
-        </div>
+        <Job job={{
+          title: "IT Technician", company: "Amentum", location: "Grand Junction, CO", dateStart: "Aug 2025", dateEnd: null, bullets: [
+            { id: 0, text: "Deployed Windows and iOS devices for an organization of 350+ users with varied configuration requirements" },
+            { id: 1, text: "Responded to user questions and provided solutions to software and hardware problems in person and remotely" },
+            { id: 2, text: "Developed and maintained PowerShell scripts to streamline device deployments" },
+            { id: 3, text: "Collaborated with team members to document solutions and share best practices" },
+          ]
+        }} />
+        <Job job={{
+          title: "Campus Missionary", company: "FOCUS", location: "Grand Junction, CO", dateStart: "May 2022", dateEnd: "Jun 2025", bullets: [
+            { id: 0, text: "Mentored 100+ college students in spiritual growth, leadership and personal excellence" },
+            { id: 1, text: "Built and maintained a dynamic donor-facing newsletter website to increase engagement and retention" },
+            { id: 2, text: "Secured full funding for role ($140K+ in 3 years) by cultivating relationships with 60+ recurring donors" },
+          ]
+        }} />
+        <Job job={{
+          title: "Coding Instructor", company: "Code Ninjas", location: "San Diego, CA / Remote", dateStart: "Jun 2021", dateEnd: "May 2022", bullets: [
+            { id: 0, text: "Delivered engaging coding lessons to 30+ students (ages 7-14) in a fast-paced setting" },
+            { id: 1, text: "Taught core and advanced concepts in JavaScript, HTML/CSS, C#, Java, and Dart" },
+            { id: 2, text: "Quickly adopted new technologies to meet evolving curriculum and student needs" },
+          ]
+        }} />
       </Section>
       <Section title="Technical Skills">
         <div className="my-2">
@@ -129,17 +101,49 @@ function Page({children}: {children: ReactNode}) {
   );
 }
 
-interface SectionProps {
+interface SectionInfo {
   title: string;
   children: ReactNode;
 }
 
-function Section({title, children}: SectionProps) {
+function Section({ title, children }: SectionInfo) {
   return (
     <section className="mt-5 mb-8">
       <h2 className="font-serif font-bold text-xl text-green-700">{title}</h2>
-      <hr className="border-t-2 mb-1.5"/>
+      <hr className="border-t-2 mb-1.5" />
       {children}
     </section>
+  );
+}
+
+interface ListItem {
+  id: number;
+  text: string;
+}
+
+interface JobInfo {
+  title: string;
+  company: string;
+  location: string;
+  dateStart: string;
+  dateEnd: string | null;
+  bullets: ListItem[];
+}
+
+function Job({job}: {job: JobInfo}) {
+  return (
+    <div className="my-2">
+      <div className="flex flex-row justify-between">
+        <h3 className="font-serif font-bold tracking-tight">{job.title} | {job.company} | {job.location}</h3>
+        <h3 className="font-serif font-bold tracking-tight">{job.dateStart} - {job.dateEnd? job.dateEnd : "Present"}</h3>
+      </div>
+      <ul className="list-disc list-inside">
+        {job.bullets.map(bullet =>
+          <li key={bullet.id} className="font-serif text-sm text-foreground-500 text-gray-800 dark:text-gray-200">
+            {bullet.text}
+          </li>
+        )}
+      </ul>
+    </div>
   );
 }
