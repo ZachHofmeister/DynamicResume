a308b9cacba7db4f3a8fe3685448d31c7ee501a5
ZachHofmeister <17202322+ZachHofmeister@users.noreply.github.com>
2026-07-12 18:33:06 -0600

    added Slot component to editor to test drag and drop into a component

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,15 +1,23 @@
 "use client";
 import { useState } from "react";
-import { Config, Data, Puck, Render } from "@puckeditor/core";
+import { Config, Data, Puck, Render, Slot } from "@puckeditor/core";
 
 //Define components and field types for typescript
 type Components = {
 	HeadingBlock: {
 		title: string;
 	};
+	Slot: {
+		content: Slot;
+	};
 };
 
-const config: Config<Components> = {
+type RootProps = {
+	title: string;
+	description: string;
+};
+
+const config: Config<Components, RootProps> = {
 	components: {
 		HeadingBlock: {
 			fields: {
@@ -23,11 +31,45 @@ const config: Config<Components> = {
 				title: "Hello World",
 			},
 			//Every component definition must provide a render function
-			render: ({ title }) => {
-				return <h1>{title}</h1>;
+			render: ({ title}) => {
+				return (
+					<h1>{title}</h1>
+				);
+			},
+		},
+		Slot: {
+			fields: {
+				content: {type: "slot"},
+			},
+			render: ({ content: Content }) => {
+				return (
+					<div className="p-5 inset-ring-1 inset-ring-orange-400">
+						<Content />
+					</div>
+				);
 			},
 		},
 	},
+	//Root is the top-level component in Puck, a wrapper around other components
+	root: {
+		fields: {
+			title: {type: "text"},
+			description: {type: "textarea"},
+		},
+		defaultProps: {
+			title: "title",
+			description: "desc",
+		},
+		render: ({ children, title, description }) => {
+			return (
+				<div>
+					<h1>{title}</h1>
+					<p>{description}</p>
+					{children}
+				</div>
+			);
+		},
+	}
 };
 
 //Describe the initial data
